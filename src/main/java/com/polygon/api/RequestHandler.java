7d80b68734d2e6f664d27b986249e8c491908077
NikichXP <nikichx2@gmail.com>
2017-04-18 04:06:38 +0300

    More code

--- src/main/java/com/polygon/api/RequestHandler.java
+++ src/main/java/com/polygon/api/RequestHandler.java
@@ -8,14 +8,32 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class RequestHandler {
 
 	@RequestMapping("/")
 	public void listen(HttpServletRequest req) throws IOException {
-		new BufferedReader(new InputStreamReader(req.getInputStream()))
-				.lines().reduce((s1, s2) -> s1 + s2).ifPresent(TelegramInputParser::analyze);
+		String x = new BufferedReader(new InputStreamReader(req.getInputStream(), StandardCharsets.UTF_8))
+				.lines().reduce((s1, s2) -> s1 + s2).orElse("");
+		x = new String(x.getBytes("ISO-8859-1"),"UTF-8");
+		System.out.println(x);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < x.length(); i++) {
+			if (x.charAt(i) != '\\') {
+				sb.append(x.charAt(i));
+			} else {
+				if (x.charAt(i + 1) == 'u') {
+					int charCode = Integer.parseInt(x.substring(i + 2, i + 6), 16);
+					sb.append(Character.toChars(charCode));
+				}
+				i += 5;
+			}
+		}
+		System.out.println(sb.toString());
+		TelegramInputParser.analyze(sb.toString());
+
 	}
 
 }
--- src/main/java/com/polygon/util/HttpSender.java
+++ src/main/java/com/polygon/util/HttpSender.java
@@ -5,6 +5,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
@@ -18,11 +19,28 @@ import java.util.List;
 public class HttpSender {
 
 	public static String get(String path) {
+		return query(path, null);
+	}
+
+	public static String query (String path, String[] args) {
 		CloseableHttpClient client = HttpClientBuilder.create().build();
-		HttpGet req = new HttpGet(path);
-		CloseableHttpResponse response = null;
+		HttpRequestBase arq;
 		try {
-			response = client.execute(req);
+			if (args != null) { //post
+				HttpPost post = new HttpPost(path);
+				List<NameValuePair> urlParameters = new ArrayList<>();
+				for (int i = 0; i < args.length / 2; i++) {
+						urlParameters.add(new BasicNameValuePair(args[i * 2], args[i * 2 + 1]));
+				}
+				post.setEntity(new UrlEncodedFormEntity(urlParameters, "UTF-8"));
+				arq = post;
+			} else { //get
+				arq = new HttpGet(path);
+			}
+//			arq.addHeader("charset", "utf-8");
+			arq.setHeader("charset", "utf-8");
+			CloseableHttpResponse response = client.execute(arq);
+
 			BufferedReader rd = new BufferedReader(
 					new InputStreamReader(response.getEntity().getContent()));
 
@@ -39,29 +57,7 @@ public class HttpSender {
 	}
 
 	public static String post (String path, String... args) {
-		CloseableHttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(path);
-		try {
-			CloseableHttpResponse response = null;
-			List<NameValuePair> urlParameters = new ArrayList<>();
-			for (int i = 0; i < args.length/2; i++) {
-				urlParameters.add(new BasicNameValuePair(args[i*2], args[i*2 + 1]));
-			}
-			post.setEntity(new UrlEncodedFormEntity(urlParameters));
-			response = client.execute(post);
-			BufferedReader rd = new BufferedReader(
-					new InputStreamReader(response.getEntity().getContent()));
-
-			StringBuffer result = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-			return result.toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		return query(path, args);
 	}
 
 }
